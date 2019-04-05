b7782d459e521fb997a25b7c22c763839007ba9b
hrakt <hraktorres@gmail.com>
2019-04-05 09:56:29 -0700

    Commit 3 - Added function checking for gameOver

--- js/app.js
+++ js/app.js
@@ -19,15 +19,21 @@ var arr2 = [
 const numOfShips = 6; // temporary variable that determines how many ships                          are going to be placed
 
 
+//create an object that will hold the ship and all the info about it
+
+
+
 
 //function fills the given array in the html table
 let fillTheTableWithTheArray = (arr) =>{
 
     for(let arrRow=0,tRow=1;arrRow<arr1.length,tRow<11; arrRow++,tRow++){
         for(let i = 1,j=0;i<=10,j<arr[0].length;i++,j++){
-            ($(`.${tRow}-row`).children().eq(i).html(arr[arrRow][j]));
+            ($(`.${tRow}`).children().eq(i).html(arr[arrRow][j]));
         }
     }
+
+
 }
 
 
@@ -35,14 +41,6 @@ let fillTheTableWithTheArray = (arr) =>{
 
 
 //function that returns which square was clicked
-let returnIndex= () => {
-    $("table").click(fucntion = (e) =>{
-        console.log(e.target);
-        console.log(e.target.parentElement);
-    })
-
-}
-returnIndex();
 
 
 //fucntion that positions the ship
@@ -161,12 +159,64 @@ return true;
 
 
 
+// function that hits a ship
+
+let hitShip = (arr,target) =>{
+    let col = Number((target.getAttribute('data-col'))) - 1;
+
+    let row =(target.parentElement.className).split(" "); // because we get the class name as a string we use split to get am array and get the first element of the array
+    row = Number(row[0]) - 1; 
+
+    if(arr[row][col] != 0 ){
+        arr[row][col] = -1;
+        fillTheTableWithTheArray(arr1);
+
+    }
+}
+
+var Game = {
+    tableArr: arr1, // tableArr holds the game table for us
+    gameStatus: true, // game status will indicate if the game is going or it has ended
+    gameTurn: 1, // game turn will indicate whos turn it is
+    name: "", // holds the name of the player
+    shipArr: [] // shipArr is an array that holds the arr of ships and the health of each one
+}
 
 
+// function that will determine if the game has ended or not
+// return fakse if game is not over and returns true if game is over
+
+let checkIfOver = (arr) => {
+
+    for(let i = 0; i<arr.length;i++){
+        for(let j = 0;j<arr[i].length; j++){
+            if(arr[i][j] != -1){
+                if(arr[i][j] != 0){
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+
+}
+
+
+// function that allows us to get the clicks and make the hits
+
+
+let getClick = () => {
+    $("table").click(fucntion = (e) =>{
+       hitShip(arr1,e.target);
+    })
+}
+
+
+getClick();
 placeShips(arr1);
-
-
 fillTheTableWithTheArray(arr1);
+// hitShip(arr1);
+console.log(checkIfOver(arr1));
 
 
 
